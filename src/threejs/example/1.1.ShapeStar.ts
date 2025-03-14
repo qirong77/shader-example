a84e03477875687facb4281f37a5939237aef30c
qirong77 <qirong7777@gmail.com>
2025-03-14 16:09:41 +0800

    shape star

--- src/threejs/example/1.1.ShapeStar.ts
+++ src/threejs/example/1.1.ShapeStar.ts
@@ -1,3 +1,4 @@
+import { gui } from "../common/gui";
 import { scene, THREE } from "../common/main";
 
 const vertexShader = /* glsl */ `
@@ -11,6 +12,10 @@ void main() {
 
 const fragmentShader = /* glsl */ `
 uniform float u_time;
+uniform float u_A;
+uniform float u_B;
+uniform float u_C;
+uniform float u_D;
 
 varying vec2 textureCoord;
 /* 四角星的✦实现思路
@@ -18,18 +23,14 @@ varying vec2 textureCoord;
 2. 定义两个线性函数y1 = Ax + B; y2 = Cx + D，他们两个相交的点作为一个控制点(pointX, pointY)。
 3. 判断当前的点是否在函数 y1、y2、X 轴、Y 轴组成的封闭图形里面，如果是，则为黑色，否则为白色
 */
-float A = -3.0;
-float B = 1.0;
-float C = -0.5;
-float D = 0.5;
 float starSDF(vec2 st) {
     float pointX = abs(st.x);
     float pointY = abs(st.y);
     /* 因为 st 在 0-1 的范围，所以只要判断当前点的 y 值都小于两个函数，说明在内侧 */
-    float y1X = -B / A;
-    float y2X = -D / C;
-    float y1Y = pointX * A  + B;
-    float y2Y = pointX * C + D;
+    float y1X = -u_B / u_A;
+    float y2X = -u_D / u_C;
+    float y1Y = pointX * u_A  + u_B;
+    float y2Y = pointX * u_C + u_D;
     bool isInsideY1 = pointX <= y1X && pointY <= y1Y; 
     bool isInsideY2 = pointX <= y2X && pointY <= y2Y;
     if (isInsideY1 || isInsideY2) {
@@ -54,7 +55,19 @@ const material = new THREE.ShaderMaterial({
     fragmentShader,
     side: THREE.DoubleSide,
     transparent: true,
+    uniforms: {
+        u_time: { value: 0 },
+        u_A: { value: -3.0 },
+        u_B: { value: 1.0 },
+        u_C: { value: -0.5 },
+        u_D: { value: 0.4 }
+    }
 });
 
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
+const folder = gui.addFolder("shape-star");
+folder.add(material.uniforms.u_A, "value", -5, 5, 0.1).name("A");
+folder.add(material.uniforms.u_B, "value", -2, 2, 0.1).name("B");
+folder.add(material.uniforms.u_C, "value", -5, 5, 0.1).name("C");
+folder.add(material.uniforms.u_D, "value", -2, 2, 0.1).name("D");
